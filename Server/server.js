485587f15e2a36e273eed16e1a592b13d3440819
Thaanees-RM <thaaneesrafeek11@gmail.com>
2025-08-12 12:20:17 +0530

    server.j set up

--- Server/server.js
+++ Server/server.js
@@ -1,4 +1,4 @@
-import express from 'express';
+kkimport express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
@@ -7,12 +7,11 @@ import formRoutes from './routes/formRoutes.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use('/uploads', express.static('uploads'));
+app.use('/uploads', express.static('uploads')); // Be careful with uploads on Vercel
 
 // Routes
 app.use('/api', formRoutes);
@@ -20,10 +19,14 @@ app.get('/', (req, res) => {
   res.send('Welcome to the API');
 });
 
-// DB Connect
+// Connect to MongoDB once when the function is cold-started
 mongoose.connect(process.env.MONGO_URI)
-.then(() => {
-  console.log('Connected to MongoDB');
-  app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
-})
-.catch((err) => console.error(err));
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => console.error(err));
+
+// **Do not call app.listen() on Vercel serverless!**
+// Instead, export the app for serverless handler:
+export default app;
+
